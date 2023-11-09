c712175d53df3b46c3f4cca6aa84fdc92eb51325
Sebastian Gomez <sebastian.gomez@ubisoft.com>
2023-11-09 18:51:35 -0500

    New unset command

---
+++ src/unset.ps1
@@ -0,0 +1,8 @@
+﻿Set-StrictMode -Off;
+
+if (!$args) { "usage: unset <variable>"; exit 1 }
+
+$name = $args[0]
+if ([Environment]::GetEnvironmentVariable($name, 'Process')) {
+  Remove-Item Env:$name
+}
